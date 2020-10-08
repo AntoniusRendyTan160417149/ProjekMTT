49fd57be8281f7a9cb2557d65dedf9403d077309
alvancahya38 <alvancahya38@gmail.com>
2020-10-08 23:52:37 +0700

    Update sold.php
    Menambahkan beberapa kodingan

--- sold.php
+++ sold.php
@@ -5,4 +5,15 @@ $iduser = $_GET['iduser_owner'];
 $sold = 'SOLD';
 $mysqli = new mysqli("localhost", "root", "", "mybidding");
 $stmt = $mysqli->query("UPDATE items SET status = '".$sold."' WHERE iditem = '".$iditem."'");
+echo $iditem. $iduser'';
 
+if($stmt == TRUE)
+{
+	header("location:detail.php?iduser_owner=".$iduser."&iditem=".$iditem."");
+}
+else
+{
+	echo "Gagal Update";
+}
+
+?>
